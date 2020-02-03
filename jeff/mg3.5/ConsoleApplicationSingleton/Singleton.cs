7f89e9bbdca41fa582d7ff84eac000a54a61ad7d
dsp56004 <onlyatest@gmail.com>
2020-02-03 12:21:25 -0600

    Update Singleton.cs
    Adds comments

--- jeff/mg3.5/ConsoleApplicationSingleton/Singleton.cs
+++ jeff/mg3.5/ConsoleApplicationSingleton/Singleton.cs
@@ -8,14 +8,18 @@ namespace ConsoleApplicationSingleton
 {
     public class Singleton
     {
+        //Private Static instance
         private static Singleton instance;
 
+        //Private Constructor
         private Singleton() { }
 
+        //Accessible Public instance
         public static Singleton Instance
         {
             get
             {
+                //Lazy Load
                 if (instance == null)
                 {
                     instance = new Singleton();
