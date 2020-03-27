25d7edfb4f8b1e08f46ab82c6fe5455fa44332b3
Jeff Meyers <jmeyers@colum.edu>
2020-03-26 21:04:25 -0500

    add MathDecorator Test

--- jeff/mg3.5/ConsoleAppMathDecorator/MathDecorator.cs
+++ jeff/mg3.5/ConsoleAppMathDecorator/MathDecorator.cs
@@ -2,9 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleAppMathDecorator
+namespace DecoratorSample
 {
-    class MathDecorator
+    class MathDecorator : IMathComponent
     {
+        List<IMathComponent> Maths;
+
+        int solution;
+
+        public MathDecorator()
+        {
+            Maths = new List<IMathComponent>();
+        }
+
+        public int Calculate(int input)
+        {
+            foreach (var item in Maths)
+            {
+                solution = item.Calculate(solution);
+            }
+            return solution;
+        }
+
+        public void AddComponent(IMathComponent compenent)
+        {
+            this.Maths.Add(compenent);
+        }
+
+        public void RemoveComponetn(IMathComponent component)
+        {
+            this.Maths.Remove(component);
+        }
     }
 }
---
+++ jeff/mg3.5/UnitTestMathDecorator/UnitTestMathCommand.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecoratorSample;
+
+namespace UnitTestMathDecorator
+{
+    [TestClass]
+    public class UnitTestMathCommand
+    {
+        [TestMethod]
+        public void TestMathCommand()
+        {
+            //Arrange
+            
+
+            //Act
+
+            //Assert
+        }
+    }
+}
