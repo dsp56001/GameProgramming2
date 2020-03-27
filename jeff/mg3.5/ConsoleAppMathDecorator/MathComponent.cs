ca5f8f678e0caf2babef04fbcf288f6393d3642a
Jeff Meyers <jmeyers@colum.edu>
2020-03-26 20:59:19 -0500

    Console Decorator
    staring decorator

---
+++ jeff/mg3.5/ConsoleAppMathDecorator/MathComponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppMathDecorator
+{
+    
+    interface IMathComponent
+    {
+        int Calculate(int input);
+    }
+    
+    abstract class MathComponent : IMathComponent
+    {
+        public virtual int Calculate(int input)
+        {
+            return input;
+        }
+
+    }
+
+    class AddOne : MathComponent
+    {
+        public override int Calculate(int input)
+        {
+            return base.Calculate(input) + 1;
+        }
+    }
+
+    class AddTwo : MathComponent
+    {
+        public override int Calculate(int input)
+        {
+            return base.Calculate(input) + 2;
+        }
+    }
+
+    class AddThree : MathComponent
+    {
+        public override int Calculate(int input)
+        {
+            return base.Calculate(input) + 3;
+        }
+    }
+}
