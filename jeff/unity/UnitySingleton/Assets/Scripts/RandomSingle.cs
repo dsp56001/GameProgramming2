3bff677a200706f5160d8ef41b9c321c192b408a
Jeff Meyers <jmeyers@colum.edu>
2021-01-04 07:46:03 -0600

    singleton old way
    old way with links to lazy way
    randomsingle sample with right and wrong implemtations

---
+++ jeff/unity/UnitySingleton/Assets/Scripts/RandomSingle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Shows the right and wrong way to use a singleton
+/// </summary>
+public class RandomSingle : Singleton<RandomSingle>
+{
+
+    public int Rand;
+    public Text text;
+
+    //Only for the Demo will use the wrong value if isRight = false
+    public bool isRight;
+
+    protected RandomSingle() { } //unity won't use constuctor be safe and make use nothing else does
+
+    void Start()
+    {
+        UpdateRandom();
+    }
+
+    void Update()
+    {
+        if (isRight)
+            this.text.text = RandomSingle.Instance.Rand.ToString();
+        else
+            this.text.text = Rand.ToString(); //Wrong this is the local instance.
+    }
+
+    public void UpdateRandom()
+    {
+        //Right
+        RandomSingle.Instance.Rand = Random.Range(0, 10);
+
+        UpdateRandomWrong();
+    }
+
+    public void UpdateRandomWrong()
+    {
+        //Wrong
+        Rand = Random.Range(0, 10);
+    }
+
+}
---
+++ jeff/unity/UnitySingleton/Assets/Scripts/RandomTextBoxDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class RandomTextBoxDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RandomSingle rs;
+
+    public Text text;
+    public UnityAction UpdateAction;
+
+    [SerializeField]
+    private int randSingle;
+
+    public int RandSingle
+    {
+        get { return randSingle = rs.Rand; }
+    }
+
+    void Start()
+    {
+        rs = this.gameObject.AddComponent<RandomSingle>();
+        UpdateAction += UpdateRandom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = rs.Rand.ToString();
+    }
+
+    public void UpdateRandom()
+    {
+        rs.UpdateRandom();
+    }
+}
